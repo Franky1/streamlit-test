5f03d5a14124b6235ab58fc67a6f5edc57639ad8
Florian Bernard <40437861+FlorianBrnrd@users.noreply.github.com>
2022-03-21 19:34:31 +0100

    Update test.py

--- web-app/test.py
+++ web-app/test.py
@@ -3,16 +3,14 @@ import streamlit as st
 import os
 
 filepath2 = '/app/streamlit-test/web-app/src/exon_coordinates.tsv'
-filepath3 = 'app/streamlit-test/web-app/src/exon_coordinates.tsv'
 
 
-st.write(os.getcwd())
-
-for path in [filepath2,filepath3]:
+for path in [filepath2]:
 
     st.write(path)
     table = pd.read_csv(path, sep='\t')
-    st.write(table)
+    if len(table)>10:
+        st.write('yay!')
     
     
 
