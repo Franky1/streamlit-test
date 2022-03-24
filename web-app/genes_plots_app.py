8fd29a155c53dbef2abaea8bb0d4198815a2f1b4
Florian Bernard <40437861+FlorianBrnrd@users.noreply.github.com>
2022-03-24 18:58:23 +0100

    test

--- web-app/genes_plots_app.py
+++ web-app/genes_plots_app.py
@@ -53,8 +53,9 @@ def app_settings():
 
     hide_streamlit_style = """
                 <style>
-                #MainMenu {visibility: hidden;}
+                MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
+                header {visibility: hidden;}
                 .appview-container .main .block-container {max-width: 1000px; padding-top: 2rem; padding-right: 1.5rem; padding-left: 1.5rem; padding-bottom: 2rem;}
                 </style>
                 """
