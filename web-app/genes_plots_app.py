64af44810d9a893589f3900c51e7a0120730260b
Florian Bernard <40437861+FlorianBrnrd@users.noreply.github.com>
2022-03-28 13:58:04 +0200

    test

--- web-app/genes_plots_app.py
+++ web-app/genes_plots_app.py
@@ -6,6 +6,14 @@ def main():
     # general settings
     app_settings()
 
+    # initialization for showing example gene on session start
+    if 'gene' not in st.session_state:
+        st.session_state['gene'] = 'lev-11'
+
+    if 'refgene' not in st.session_state:
+        st.session_state['refgene'] = 'Y105EB.1'
+
+
     show_title()
 
     # open ref files and cache them
@@ -19,21 +27,26 @@ def main():
     # chose plot settings
     atg_option = plot_settings()
 
-    # TO DO: add checking gene name input
-    if valid is True :
+    # checking gene name input
+    if valid is not True :
+        gene = st.session_state['gene']
+        refgene = st.session_state['refgene']
 
-        display_gene_infos(gene, refgene)
 
-        # generate and show gene plot
-        gene_plot = plot_gene_start(dataset, gene, genes, exons, ATGPOSITIONS, show_atg=atg_option)
-        config = {'displayModeBar': False}
-        st.plotly_chart(gene_plot, use_container_width=True, config=config)
+    display_gene_infos(gene, refgene)
 
-        # add legend below
-        show_legend()
+    # generate and show gene plot
+    gene_plot = plot_gene_start(dataset, gene, genes, exons, ATGPOSITIONS, show_atg=atg_option)
+    config = {'displayModeBar': False}
+    st.plotly_chart(gene_plot, use_container_width=True, config=config)
 
-        # show only if plot was generated
-        download_plotly_static(gene_plot, gene, refgene)
+    # add legend below
+    show_legend()
+
+    # show only if plot was generated
+    download_plotly_static(gene_plot, gene, refgene)
+
+
 
     # show app/labs infos
     #show_biorxiv()
